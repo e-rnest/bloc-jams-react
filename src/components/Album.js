a0a107e44ecb2296b00f605aba2a7d5d2b8ebbde
Ernest <ernestc@gmail.com>
2018-06-14 00:51:54 -0700

    checkpoint-09-rangeinputs pre-exercise

--- src/components/Album.js
+++ src/components/Album.js
@@ -13,6 +13,8 @@ class Album extends Component {
     this.state = {
       album: album,
       currentSong: album.songs[0],
+      currentTime: 0,
+      duration: album.songs[0].duration, 
       isPlaying: false,
     };
 
@@ -89,6 +91,28 @@ class Album extends Component {
     this.play();
   }
 
+  componentDidMount() {
+    this.eventListeners = {
+      timeupdate: (e) => { this.setState({ currentTime: this.audioElement.currentTime }) },
+      durationchange: (e) => {this.setState({ duration: this.audioElement.duration }) },
+    }
+    this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
+    this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
+  }
+
+  componentWillUnmount() {
+    this.audioElement.src = null;
+    this.audioElement = null;
+    this.audioElement.removeEventListener('timeupdate', this.eventListeners.timeupdate);
+    this.audioElement.removeEventListener('durationchange', this.eventListeners.durationchange);
+  }
+
+  handleTimeChange(e) {
+    const newTime = this.audioElement.duration * e.target.value;
+    this.audioElement.currentTime = newTime;
+    this.setState({ currentTime: newTime });
+  }
+
   render() {
     return (
       <section className="album">
@@ -124,6 +148,9 @@ class Album extends Component {
             handleSongClick={() => this.handleSongClick(this.state.currentSong)}
             handlePrevClick={() => this.handlePrevClick()}
             handleNextClick={() => this.handleNextClick()}
+            handleTimeChange={(e) => this.handleTimeChange(e)}
+            currentTime={this.state.currentTime}
+            duration={this.state.duration}
           />
       </section>
     );
